8e3b7ec4a333b431df377b951bf16e59e46cca78
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-10-12 17:54:27 +0200

    add jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,32 @@
 @extends('layouts.app')
 
 @section('main-content')
-<section class="contianer mt-5">
-   <div class="row g-4">
-    @foreach($comics as $comic)
-    <div class="col-md-2">
-        <div class="card">
-            <div class="d-flex justify-content-center">
-                <img class="custom-img" src="{{$comic ['thumb'] }}" alt="">
-            </div>
-            <div class="d-flex justify-content-center">
-                <h4>{{$comic ['title'] }}</h4>
+<section>
+    <div class="jumbtron">
+        <img src="{{Vite::asset('public\images\jumbotron.jpg')}}" alt="">
+    </div>
+    <div class="container mt-5">
+        <div class="current">
+            <span>CURRENT SERIES</span>
+        </div>
+       <div class="row g-4">
+        @foreach($comics as $comic)
+        <div class="col-md-2">
+            <div class="card_comics">
+                <div class="d-flex justify-content-center">
+                    <img class="custom-img" src="{{$comic ['thumb'] }}" alt="">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <h4>{{$comic ['title'] }}</h4>
+                </div>
             </div>
         </div>
+        @endforeach
+       </div>
+       <div class="button_comics">
+          <span>LOAD MORE</span>
+      </div>
     </div>
-    @endforeach
-   </div>
 </section>
+
 @endsection
